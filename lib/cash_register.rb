1c1386dd7385e789da1798e4766f1fbf15992435
Chhaian Pin <chhaianpin@hotmail.com>
2019-02-21 05:28:39 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  attr_accessor :discount, :total, :quantity
+  # attr_reader :total
+  
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+    @items = []
+  end
+  
+  def add_item(title, price, quantity=1)
+    @items << title
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    @total *= (100.0 - @discount) / 100.0
+  end
+  
+end
